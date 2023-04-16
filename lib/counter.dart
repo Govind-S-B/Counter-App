55940492805385a390efe782baf89b4c0ae86a78
Govind_SB <b.s.dnivog@gmail.com>
2023-04-16 04:45:11 +0000

    Styling counter widget

--- lib/counter.dart
+++ lib/counter.dart
@@ -14,23 +14,32 @@ class _CounterWidgetState extends State<CounterWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          border: Border.all(
-            width: 2,
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Container(
+        decoration: BoxDecoration(
+            border: Border.all(
+              width: 2,
+            ),
+            borderRadius: BorderRadius.circular(5)),
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Row(
+            children: [
+              Expanded(child: Text(widget.task)),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.black,
+                ),
+                onPressed: () {
+                  count++;
+                  setState(() {});
+                },
+                child: Text("$count"),
+              )
+            ],
           ),
-          borderRadius: BorderRadius.circular(5)),
-      child: Row(
-        children: [
-          Expanded(child: Text(widget.task)),
-          ElevatedButton(
-            onPressed: () {
-              count++;
-              setState(() {});
-            },
-            child: Text("$count"),
-          )
-        ],
+        ),
       ),
     );
   }

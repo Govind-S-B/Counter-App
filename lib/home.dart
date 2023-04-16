a4c3e6155a00c1d59046ea947ed21ca7f3f26258
Govind_SB <b.s.dnivog@gmail.com>
2023-04-16 01:41:39 +0000

    Basic home page with dialog box

---
+++ lib/home.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List widgetList = []; // holds the list of widgets
+  late TextEditingController fieldCtrl;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    fieldCtrl = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    fieldCtrl.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("CounterApp"),
+      ),
+      body: Container(
+          child: widgetList.isEmpty
+              ? Text("Add a new task to count")
+              : Text("scrollable list of counter widgets")),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () async {
+          var name = await openDialog();
+          if (name == null || name.isEmpty) return;
+          // make a counter widget with given name
+          // add widget to list
+          // set state to reload new list on screen
+        },
+      ),
+    );
+  }
+
+  Future<String?> openDialog() => showDialog<String>(
+      context: context,
+      builder: (context) => AlertDialog(
+            title: Text("Enter Task"),
+            content: TextField(
+              controller: fieldCtrl,
+            ),
+            actions: [
+              TextButton(
+                child: Text("Add"),
+                onPressed: () {
+                  Navigator.of(context).pop(fieldCtrl.text);
+                },
+              ),
+            ],
+          ));
+}

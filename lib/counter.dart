35f1349a0c18b3c103de4a63c6e9a1de18c10138
Govind_SB <b.s.dnivog@gmail.com>
2023-04-16 01:42:57 +0000

    create custom widget

---
+++ lib/counter.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class CounterWidget extends StatefulWidget {
+  final String task;
+
+  const CounterWidget({Key? key, required this.task}) : super(key: key);
+
+  @override
+  State<CounterWidget> createState() => _CounterWidgetState();
+}
+
+class _CounterWidgetState extends State<CounterWidget> {
+  int count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          border: Border.all(
+            width: 2,
+          ),
+          borderRadius: BorderRadius.circular(5)),
+      child: Row(
+        children: [
+          Expanded(child: Text("${widget.task}")),
+          ElevatedButton(
+            onPressed: () {
+              count++;
+              setState(() {});
+            },
+            child: Text("$count"),
+          )
+        ],
+      ),
+    );
+  }
+}
